3ae39990e5ff60708f96baca87239660c0f21d8d
Remco van 't Veer <remco@remworks.net>
2025-02-28 09:58:35 +0100

    Use fixed version
    See also: https://github.com/rm-hull/nvd-clojure/pull/183

--- nvd-clojure.sh
+++ nvd-clojure.sh
@@ -1,9 +1,16 @@
 #!/bin/sh -e
 
-clojure -Ttools list | grep -q '^nvd ' ||
+# See also: https://github.com/rm-hull/nvd-clojure/pull/183
+NVD_CLOJURE_REPO=https://github.com/jomco/nvd-clojure.git
+NVD_CLOJURE_COMMIT=88b2150908fc42b5476ec5dddc7558457fa28d3e
+
+if clojure -Ttools show '{:tool nvd}' | grep -q $NVD_CLOJURE_COMMIT; then
+    :
+else
     clojure -J-Dclojure.main.report=stderr \
             -Ttools install \
-            nvd-clojure/nvd-clojure '{:mvn/version "RELEASE"}' :as nvd
+            nvd-clojure/nvd-clojure "{:git/url \"${NVD_CLOJURE_REPO}\" :git/sha \"${NVD_CLOJURE_COMMIT}\"}" :as nvd
+fi
 
 ARGS=''
 [ -f .nvd-config.json ] && ARGS=':config-filename ".nvd-config.json"'
@@ -16,7 +23,6 @@ fi
 
 clojure -J-Dclojure.main.report=stderr \
         -J-Dorg.slf4j.simpleLogger.defaultLogLevel=error \
-        -Sdeps '{:deps {org.owasp/dependency-check-maven {:mvn/version "10.0.2"}}}' \
         -Tnvd nvd.task/check \
         :classpath "\"$CP\"" \
         $ARGS
